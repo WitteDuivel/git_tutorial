26f1cef166e621cd9a0bb54829507d58744fb1f1
WitteDuivel <sugar4uwastaken@gmail.com>
2022-10-26 12:57:26 +0530

    Integrated the add_nums method into the main file.

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from sum import add_nums
 print("Hello, World!")
 name = input("What is your name?\n")
 print(f"Hello {name}")
+
+the_nums = []
+
+while (num := input(">>> ")) != "q":
+    the_nums.append(num)
+
+print(add_nums(the_nums))
